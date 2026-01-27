c4b54059bbfc7d2f5ec6d276b79645c3e93eddcd
VivekTechSolutions <vivek.kumar@logicoy.com>
2026-01-27 18:30:11 +0530

    Create and initialize online_retail_db database

---
+++ online_retail_db.sql
@@ -0,0 +1,5 @@
+-- Create the database only if it does not already exist
+CREATE DATABASE IF NOT EXISTS online_retail_db;
+
+-- Switch to the online_retail_db database for subsequent operations
+USE online_retail_db;
